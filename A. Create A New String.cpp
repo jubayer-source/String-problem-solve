1d0913c46e9940340e6934044d370907b6742306
Md. Jubayer Ahmad <mdjubayerahmad16@gmail.com>
2024-10-24 01:04:35 +0600

    Add files via upload

---
+++ A. Create A New String.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string S, T;
+    cin >> S >> T;
+
+    cout << S.length() << " " << T.length() <<endl;
+
+    cout << S << " " << T <<endl;
+
+
+    return 0;
+}
